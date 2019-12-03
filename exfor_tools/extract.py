12e08bdf533014771fdb8033d4820cd4c4ee37fe
fzeiser <fzeiser@physnet.uni-hamburg.de>
2019-12-03 19:07:21 +0200

    refactor

--- exfor_tools/extract.py
+++ exfor_tools/extract.py
@@ -33,31 +33,32 @@ def read_exfor(fname):
     return data
 
 
-data = read_exfor("X4_exp.txt")
+if __name__ == "__main__":
+    data = read_exfor("X4_exp.txt")
 
-df = data.groupby("year, auth")
+    df = data.groupby("year, auth")
 
-outdir = "split"
-try:
-    os.mkdir(outdir)
-except FileExistsError:
-    pass
+    outdir = "split"
+    try:
+        os.mkdir(outdir)
+    except FileExistsError:
+        pass
 
-nfigs = int(np.ceil(len(df)/5))
-fig, axes = plt.subplots(nfigs, 1)
-marker = itertools.cycle(('>', '+', '<', 'o', '*'))
-for i, (name, group) in enumerate(df):
-    ax = axes[i % nfigs] if nfigs > 0 else axes
-    ax.errorbar(group["x"], group["y"], yerr=group["dy"], label=name, fmt="o",
-                alpha=0.5, markerfacecolor="None",
-                markersize=2, marker=next(marker))
-    outname = name.replace(" ", "_")
-    group.to_csv(f"{outdir}/{outname}", sep="\t")
+    nfigs = int(np.ceil(len(df)/5))
+    fig, axes = plt.subplots(nfigs, 1)
+    marker = itertools.cycle(('>', '+', '<', 'o', '*'))
+    for i, (name, group) in enumerate(df):
+        ax = axes[i % nfigs] if nfigs > 0 else axes
+        ax.errorbar(group["x"], group["y"], yerr=group["dy"], label=name, fmt="o",
+                    alpha=0.5, markerfacecolor="None",
+                    markersize=2, marker=next(marker))
+        outname = name.replace(" ", "_")
+        group.to_csv(f"{outdir}/{outname}", sep="\t")
 
 
-    ax.set_yscale("log")
-    ax.set_xlim(0, 20)
-    ax.set_ylim(1e0, 1e2)
-    ax.legend()
+        ax.set_yscale("log")
+        ax.set_xlim(0, 20)
+        ax.set_ylim(1e0, 1e2)
+        ax.legend()
 
-plt.show()
+    plt.show()
